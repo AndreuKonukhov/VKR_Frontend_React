b92149724ecf39fe3b6c7c2855e9868679abb70d
Andreu <akonuxov@list.ru>
2023-11-14 10:38:18 +0700

    added

--- src/redux/navbar-reducer.js
+++ src/redux/navbar-reducer.js
@@ -3,9 +3,9 @@ const UPDATE_NEW_SECH_TEXT = 'UPDATE-NEW-SECH-TEXT';
 
 let initialState = {
     seches: [
-        {nameSech: "Братск - Иркутск", countNN: 4},
-        {nameSech: "Ангара - Запад", countNN: 1},
-        {nameSech: "Челябинское", countNN: 2},
+        {id: 1, nameSech: "Братск - Иркутск", countNN: 4},
+        {id: 2, nameSech: "Ангара - Запад", countNN: 1},
+        {id: 3, nameSech: "Челябинское", countNN: 2},
     ],
     newSechText: 'по умолчанию'
 };
@@ -13,17 +13,24 @@ let initialState = {
 const navbarReducer = (state = initialState, action) => {
    
     switch(action.type) {
-        case ADD_SECH:
+        case ADD_SECH:{
             let newSech = {
+                id: 4,
                 nameSech: state.newSechText, 
                 countNN: 2
             };
-            state.seches.push(newSech);
-            state.newSechText = 'сброс';
-            return state;
-        case UPDATE_NEW_SECH_TEXT:
-            state.newSechText = action.newText;
-            return state;
+            return {
+                ...state,
+                seches: [...state.seches, newSech],
+                newSechText: 'Сброс'
+            };
+        }
+        case UPDATE_NEW_SECH_TEXT: {
+            return {
+                ...state,
+                newSechText: action.newText
+            }
+        }
         default:
             return state;
     }
